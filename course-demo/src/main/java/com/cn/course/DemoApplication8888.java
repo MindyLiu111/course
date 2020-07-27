2d9cc959f010e01fcb386f0f8c26b436d9bf8a8a
mindy <1073980509@qq.com>
2020-07-27 23:43:52 +0800

    added demo

--- course-demo/src/main/java/com/cn/course/DemoApplication8888.java
+++ course-demo/src/main/java/com/cn/course/DemoApplication8888.java
@@ -4,9 +4,11 @@ package com.cn.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan("com.cn.course.dao")
 public class DemoApplication8888 {
 
     public static void main(String[] args) {
