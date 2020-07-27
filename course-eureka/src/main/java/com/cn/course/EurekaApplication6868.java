c89a777016eff807ce56448cb0d8ffaaf9a03fb2
mindy <1073980509@qq.com>
2020-07-27 09:47:37 +0800

    add 01

---
+++ course-eureka/src/main/java/com/cn/course/EurekaApplication6868.java
@@ -0,0 +1,15 @@
+package com.cn.course;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication6868 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication6868.class);
+    }
+}
